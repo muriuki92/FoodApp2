7d9f0d6c80ff6cf2612248c7d513085943725fc2
Food App <muriukisheila33@gmail.com>
2022-08-29 11:17:33 +0300

    git commit"first commit"

--- app/src/main/java/com/example/foodapp2/IntroActivity.kt
+++ app/src/main/java/com/example/foodapp2/IntroActivity.kt
@@ -1,11 +1,25 @@
 package com.example.foodapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils.loadAnimation
+import android.widget.ImageView
+import com.google.android.material.animation.AnimationUtils
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+         val backgrounding: ImageView = findViewById(R.id.slide)
+        val animation = AnimationUtils.loadAnimation(this,R.anim.slide)
+        backgrounding.startAnimation(animation)
+        Handler().postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
     }
-}
+
+    }
